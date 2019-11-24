1f2f21dcb802abb746c91c1a3feacc51e0703bd1
Olivia Davis <olivia.davis593@gmail.com>
2019-11-24 01:28:17 -0600

    added scroll navigation to files

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('.navigation a').on('click', function (e) {
+          if (this.hash !== '') {
+             e.preventDefault();
+             const hash = this.hash;
+             $('html, body')
+              .animate({
+                scrollTop: $(hash).offset().top
+              },800);
+           }
+         });
+});
